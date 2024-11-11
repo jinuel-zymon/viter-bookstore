7f11af1a07b02f26db8060328330e673f0c75f09
xtpen <bilogmanuel.educ@gmail.com>
2024-11-11 15:58:48 +0800

    first commit

---
+++ src/components/helpers/functions-general.jsx
@@ -0,0 +1,2 @@
+export const projectURL = "http://localhost/viter-bookstore";
+export const imgPath = "http://localhost/viter-bookstore/public/img";
---
+++ src/components/pages/frontend/Header.jsx
@@ -0,0 +1,85 @@
+import { ShoppingBag } from 'lucide-react'
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { imgPath } from '../../helpers/functions-general'
+
+const Header = () => {
+  const [scrollPosition, setScrollPosition] = React.useState(0)
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  })
+
+
+
+  const handleOpenCart = () => dispatch(setIsAdd(true));
+  return (
+    <>
+    <header
+    className={`px-4 py-2 transition-all z-50 bg-gray-600 ${scrollPosition > 100 ? "fixed top-0 left-0 w-full bg-black bg-opacity-70" : ""}`}
+  >
+    <div className="container">
+
+      <div className="flex justify-between items-center ">
+        <div className="">
+          <Link to="/">
+            <img
+              src={`${imgPath}/logo.png`}
+              alt=""
+              className= "w-[80px]"
+            />
+          </Link>
+        </div>
+
+
+        <ul className="flex gap-10 items-center">
+      
+
+
+          <li>
+            <NavLink to="/home" className="text-base font-bold text-white">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/location" className="text-base font-bold text-white">
+              Location
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="text-base font-bold text-white">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog" className="text-base font-bold text-white">
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+
+
+        <button className="relative" >
+          <ShoppingBag stroke={"#fff"} />
+        </button>
+      </div>
+    </div>
+  </header>
+    </>
+  )
+}
+
+
+export default Header
+
+
+
+
